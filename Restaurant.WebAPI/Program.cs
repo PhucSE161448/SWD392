ece4c155b7d7f47e3eba268d61b928b3993e23f9
Phúcc <lamminhphuc285@gmail.com>
2024-03-01 12:07:12 +0700

    a

--- Restaurant.WebAPI/Program.cs
+++ Restaurant.WebAPI/Program.cs
@@ -69,7 +69,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c => {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json","MixFood V1"),
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MixFood V1");
         c.RoutePrefix = string.Empty;
     });
 }
